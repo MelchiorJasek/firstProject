6f4e79e34afa46722178803c3551b256502a290a
Melchior Jasek <112726800+MelchiorJasek@users.noreply.github.com>
2024-03-25 22:17:21 +0100

    commit test drag adn drop

--- tests/testFromCourse.spec.ts
+++ tests/testFromCourse.spec.ts
@@ -2,6 +2,7 @@ import {test, expect} from '@playwright/test';
 
 test('drag and drops', async ({page}) => {
     await page.goto ('https://www.globalsqa.com/demo-site/draganddrop/')
+    await page.getByLabel('Consent', { exact: true }).click();
     const frame = page.frameLocator('[rel-title="Photo Manager"] iframe')
     await frame.locator('li', {hasText:"High Tatras 2"}).dragTo(frame.locator('#trash')); // prznosimy obrazek do smietnika
 
